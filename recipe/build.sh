7221a229b9ba6eef11605a56c7203132fafddea1
Andrew Howe <ahowe@anaconda.com>
2026-06-30 10:57:00 -0400

    adding osx and some cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CXXFLAGS="$CXXFLAGS -isystem $PREFIX/include"
 if [[ $(uname) == "Linux" ]]; then
     export CXXFLAGS="$CXXFLAGS -Wno-dangling-reference"
     # Disable LTO to avoid linker issues with tvm_ffi shared library
-    export CXXFLAGS="$CXXFLAGS -isystem $PREFIX/include -fno-lto"
+    export CXXFLAGS="$CXXFLAGS -fno-lto"
     export LDFLAGS="$LDFLAGS -fno-lto"
 else
     export CXXFLAGS="$CXXFLAGS -Wno-deprecated-literal-operator"
